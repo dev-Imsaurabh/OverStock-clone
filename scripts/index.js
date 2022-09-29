f65e4c1b5513f48c338e8c9b7a417bf5a4af2766
dev-Iamsaurabh <sau4478@gmail.com>
2022-09-29 22:55:31 +0530

    day-3 completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 document.querySelector("#leftSlide").addEventListener("click", slideLeft);
 
 document.querySelector("#rightSlide").addEventListener("click", rightSlide);
@@ -16,8 +17,21 @@ document.querySelector("#account").addEventListener("click", function (event) {
   window.location.href = "signup.html";
 });
 
-document.querySelector("#homepage").addEventListener("click", function (event) {
-  window.location.href = "index.html";
-});
 
 
+  function isUserAvailable(){
+    let obj = JSON.parse(localStorage.getItem("loggedUser"))||{}
+    let count =0
+   for(let x in obj){
+    count++
+   }
+  
+   if(count>0){
+    let name = obj.email
+    name = name.substring(0,7)
+    document.querySelector("#username").innerText=name
+   }
+  
+  }
+
+  
